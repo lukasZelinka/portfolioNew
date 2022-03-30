832343190a2990ff82459f55c30904029216f6f8
lukasZelinka <lukas.zelinka@gmail.com>
2022-03-30 12:25:01 +0200

    preloader

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import RingLoader from "react-spinners/RingLoader";
 
 function App() {
-  return <p className="color">blue</p>;
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2400);
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <div className="centerWrapper">
+          <RingLoader color={"#9DAAF2"} loading={loading} size={150} />
+        </div>
+      ) : (
+        <p>App</p>
+      )}
+    </>
+  );
 }
 
 export default App;
